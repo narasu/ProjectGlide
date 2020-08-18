9f03cf1df4475126bc468406b9ba02f7db3ffe53
narasu <58448900+narasu@users.noreply.github.com>
2020-08-18 17:01:00 +0200

    added Z rotation to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,17 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private InputMaster controls;
-    [SerializeField] private float jetSpeed = 5.0f;
+    [SerializeField] private float jetSpeed = 50.0f;
     [SerializeField] private float aimSpeed = 5.0f;
-    private Vector3 movement;
 
     private Vector3 aimDirection;
+    public bool invertY;
 
     Quaternion rotation;
     Quaternion prevRotation;
 
-    [SerializeField] private float rotationIntensity = 5.0f;
+    [SerializeField] private float turnIntensity = 35.0f;
+    [SerializeField] private float rollIntensity = 3.0f;
 
     private void Awake()
     {
@@ -24,7 +25,16 @@ public class Player : MonoBehaviour
 
         controls.Airship.Shoot.performed += ctx => Shoot();
 
-        controls.Airship.Aim.performed += ctx => aimDirection = ctx.ReadValue<Vector2>();
+        if (!invertY)
+        {
+            controls.Airship.Aim.performed += ctx => aimDirection = ctx.ReadValue<Vector2>();
+        }
+        else
+        {
+            controls.Airship.Aim.performed += ctx => aimDirection = new Vector3(ctx.ReadValue<Vector2>().x, -ctx.ReadValue<Vector2>().y);
+        }
+
+        
         controls.Airship.Aim.canceled += ctx => aimDirection = Vector2.zero;
 
         Cursor.lockState = CursorLockMode.Locked;
@@ -33,11 +43,18 @@ public class Player : MonoBehaviour
     private void Update()
     {
         //Debug.Log(aimDirection);
+        
         prevRotation = transform.localRotation;
-        rotation = Quaternion.Euler(new Vector3(-aimDirection.y * rotationIntensity, aimDirection.x * rotationIntensity));
+
+        float zRotation = Mathf.Atan2(-aimDirection.x, Mathf.Abs(aimDirection.y)) * rollIntensity;
+        
+        //float zRotation = Vector2.SignedAngle(Vector2.zero, aimDirection) * rollIntensity;
+
+
+        rotation = Quaternion.Euler(new Vector3(-aimDirection.y * turnIntensity, aimDirection.x * turnIntensity, zRotation));
         transform.localRotation = Quaternion.Lerp(prevRotation, rotation, 0.1f);
-        //movement = Vector3.forward * jetSpeed * Time.deltaTime + aimDirection * aimSpeed * Time.deltaTime;
-        movement = Vector3.forward * jetSpeed * Time.deltaTime;
+
+        Vector3 movement = Vector3.forward * jetSpeed * Time.deltaTime;
         transform.Translate(movement);
     }
 
