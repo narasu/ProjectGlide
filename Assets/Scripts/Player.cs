7251ea915faa42b2130b2bffb84214d425d18af2
narasu <58448900+narasu@users.noreply.github.com>
2020-08-18 15:34:49 +0200

    added rotation to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private InputMaster controls;
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jetSpeed = 5.0f;
+    [SerializeField] private float aimSpeed = 5.0f;
     private Vector3 movement;
+
     private Vector3 aimDirection;
+
+    Quaternion rotation;
+    Quaternion prevRotation;
+
+    [SerializeField] private float rotationIntensity = 5.0f;
 
     private void Awake()
     {
@@ -26,7 +33,11 @@ public class Player : MonoBehaviour
     private void Update()
     {
         //Debug.Log(aimDirection);
-        movement = Vector3.forward * speed * Time.deltaTime + aimDirection;
+        prevRotation = transform.localRotation;
+        rotation = Quaternion.Euler(new Vector3(-aimDirection.y * rotationIntensity, aimDirection.x * rotationIntensity));
+        transform.localRotation = Quaternion.Lerp(prevRotation, rotation, 0.1f);
+        //movement = Vector3.forward * jetSpeed * Time.deltaTime + aimDirection * aimSpeed * Time.deltaTime;
+        movement = Vector3.forward * jetSpeed * Time.deltaTime;
         transform.Translate(movement);
     }
 
